37e1946478874265af2c2b8a2d30172690073f42
Fabio Sisi <45483096+Fabiooo98@users.noreply.github.com>
2021-06-11 15:55:29 +0200

    first try

--- test.sh
+++ test.sh
@@ -1,4 +1,23 @@
-printf "Hello World\n"
-printf "Memory Information\n"
-pwd
-printf "123"
+cd
+# setup variables and arguments
+DIR_NAME=$1
+FILE_NAME=$2
+DIR="/home/fabio/$DIR_NAME"
+# check if arguments are given
+if [[ -z "$DIR_NAME" || -z "$FILE_NAME" ]]
+then
+    echo "Missing Arguments"
+    exit -1
+fi
+# check if directory exists, if not then create it
+if [ ! -d "$DIR" ]
+then
+    mkdir $DIR_NAME
+fi
+cd $DIR_NAME
+# check if file exists, if not then create it
+if [ ! -f "$DIR/$FILE_NAME.txt" ]
+then
+    touch "$FILE_NAME.txt"
+fi
+exit 0
