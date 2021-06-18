889d830ae16b7860ea26b53058305264c1f8a8a4
Fabio Sisi <45483096+Fabiooo98@users.noreply.github.com>
2021-06-18 14:54:18 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 cd
 # setup variables and arguments
 DIR_NAME=$1
-FILE_NAME=$2
+FILE_NAME="data"
 DIR="/home/fabio/$DIR_NAME"
 # check if arguments are given
 if [[ -z "$DIR_NAME" || -z "$FILE_NAME" ]]
@@ -16,15 +16,17 @@ then
 fi
 cd $DIR_NAME
 # check if file exists, if not then create it
-if [ ! -f "$DIR/$FILE_NAME.txt" ]
+if [ ! -f "$DIR/$FILE_NAME.csv" ]
 then
-    touch "$FILE_NAME.txt"
+    touch "$FILE_NAME.csv"
 fi
+# header line in file
+echo 'cpu_usage_pct,ram_usage_pct,network_in,network_out' >> "$FILE_NAME.csv"
 # insert CPU in file for 10 seoncds
 x=1
 while [ $x -le 10 ]
 do
-    echo `top -b -n1 | grep "Cpu(s)" | awk '{print $2 + $4}'` >> "$FILE_NAME.txt"
+    echo "`top -b -n1 | grep "Cpu(s)" | awk '{print $2 + $4}'`,`free | grep Mem | awk '{print $3/$2}'`,`ifstat -i eth0 -q 1 1 | sed -n '3 p' | awk '{print $1","$2}'`" >> "$FILE_NAME.csv"
     x=$(( $x + 1 ))
     sleep 1
 done
