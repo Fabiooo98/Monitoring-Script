ea06d5952924e2da73517a8c608d5fdac38fbdcd
Fabio Sisi <45483096+Fabiooo98@users.noreply.github.com>
2021-07-03 15:02:38 +0200

    introduced arguments, formatting

--- test.sh
+++ test.sh
@@ -1,25 +1,47 @@
 #!/bin/bash -u
-
+##############################################################
+###############		        SETUP		      ################
+##############################################################
 # Set language to make sure same separator (, and .) config is being used
 export LC_ALL=C.UTF-8
-
 # Move to home directory
 cd
 # setup variables and arguments
-# Name of the directory
-DIR_NAME=$1
-# Name of the file
-FILE_NAME="data"
-# Time window per sample
-timeWindow=10
-# Number of sampoles to be taken
-samples=1000
-
-# check if arguments are given
-if [[ -z "$DIR_NAME" || -z "$FILE_NAME" ]]
+while getopts ":f:n:a:t:" opt; do
+  case $opt in
+    n)
+    DIR_NAME=$OPTARG
+    ;;
+    a)
+    SAMPLES=$OPTARG
+    ;;
+    f)
+    FILE_NAME=$OPTARG
+    ;;
+    t)
+    TIME=$OPTARG
+  esac
+done
+# check if arguments are given, if not set default values
+# Name of the target directory
+if [ -z "$DIR_NAME" ]
 then
-    echo "Missing Arguments"
-    exit -1
+    DIR_NAME='default_DIR'
+fi
+# Amount of samples to be taken
+if [ -z "$SAMPLES" ]
+then
+    SAMPLES=10
+fi
+# Name of the target file
+if [ -z "$FILE_NAME" ]
+then
+    FILE_NAME='data'
+fi
+# Time between samples
+if [ -z "$TIME" ]
+then
+    TIME=10
 fi
 # check if directory exists, if not then create it
 if [ ! -d "$DIR_NAME" ]
@@ -35,19 +57,29 @@ then
     touch "$FILE_NAME.csv"
 fi
 # header line in file
-echo 'time,cpu_usage_pct,ram_usage_pct,network_in,network_out' >> "$FILE_NAME.csv"
-# insert monitoring dimensions in file for 10 seoncds
+echo 'time;cpu_usage_pct;ram_usage_pct;network_in;network_out' >> "$FILE_NAME.csv"
+##############################################################
+#############		   MONITORING LOOP			##############
+##############################################################
 x=1
-while [ $x -le $samples ]
+while [ $x -le $SAMPLES ]
 do
-    printf `date +'%T'";"` >> "$FILE_NAME.csv"
-    printf `top -b -n1 | grep "Cpu(s)" | awk '{print $2 + $4}'`";" >> "$FILE_NAME.csv"
-    printf `free | grep Mem | awk '{print $3/$2}'`";" >> "$FILE_NAME.csv"
-    printf `ifstat -i eth0 -q 1 1 | sed -n '3 p' | awk 'OFS=";" {print $1,$2}'` >> "$FILE_NAME.csv"
-    printf "\n" >> "$FILE_NAME.csv"
-    #up counter and wait 5 seconds
+    ##############################################################
+	#############		   DATA COLLECTION			##############
+	##############################################################
+    # Perf data collection
+    #perf stat -e "$EVENTS" --o "$TEMP_OUPUT" -a sleep "$TIME"
+    # Data extraction
+    timestamp=$(echo `date +'%T'`)
+    cpu=$(echo `top -b -n1 | grep "Cpu(s)" | awk '{print $2 + $4}'`)
+    mem=$( `free | grep Mem | awk '{print $3/$2}'`)
+    network=$(`ifstat -i eth0 -q 1 1 | sed -n '3 p' | awk 'OFS=";" {print $1,$2}'`)
+    ##############################################################
+	#############			   OUTPUT				##############
+	##############################################################
+    echo "$timestamp;$cpu;$mem;$network" >> "$FILE_NAME.csv"
+    #up counter and wait
     x=$(( $x + 1 ))
-    sleep $timeWindow
+    sleep $TIME
 done
-pwd
 exit 0
