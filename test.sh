ba24f5988040012d32624b3f53461afb0166efc1
Fabio Sisi <45483096+Fabiooo98@users.noreply.github.com>
2021-07-03 15:41:38 +0200

    added hpc

--- test.sh
+++ test.sh
@@ -48,6 +48,8 @@ if [ ! -d "$DIR_NAME" ]
 then
     mkdir $DIR_NAME
 fi
+# List of events to monitor using perf
+EVENTS=""
 # Enter target directory
 cd $DIR_NAME
 # check if file exists, if not then create it
@@ -56,8 +58,14 @@ then
     # Create the file
     touch "$FILE_NAME.csv"
 fi
+##############################################################
+#############		  OUTPUT FORMATTING  		##############
+##############################################################
+# Perf execution to get the order of the events (perf might not ouput them in the specified order)
+perf stat -e "$EVENTS" --o "$TEMP_OUTPUT" -a sleep 1
+placeholder=$(cat $TEMP_OUTPUT | cut -b 25- | cut -d " " -f 1 | tail -n +6 | head -n -3 | tr "\n" "," | sed 's/.$//')
 # header line in file
-echo 'time;cpu_usage_pct;ram_usage_pct;network_in;network_out' >> "$FILE_NAME.csv"
+echo "time;cpu_usage_pct;ram_usage_pct;network_in;network_out;${placeholder}" >> "$FILE_NAME.csv"
 ##############################################################
 #############		   MONITORING LOOP			##############
 ##############################################################
@@ -68,18 +76,18 @@ do
 	#############		   DATA COLLECTION			##############
 	##############################################################
     # Perf data collection
-    #perf stat -e "$EVENTS" --o "$TEMP_OUPUT" -a sleep "$TIME"
+    perf stat -e "$EVENTS" --o "$TEMP_OUTPUT" -a sleep "$TIME"
     # Data extraction
     timestamp=$(echo `date +'%T'`)
     cpu=$(echo `top -b -n1 | grep "Cpu(s)" | awk '{print $2 + $4}'`)
-    mem=$( `free | grep Mem | awk '{print $3/$2}'`)
-    network=$(`ifstat -i eth0 -q 1 1 | sed -n '3 p' | awk 'OFS=";" {print $1,$2}'`)
+    mem=$(echo `free | grep Mem | awk '{print $3/$2}'`)
+    network=$(echo `ifstat -i eth0 -q 1 1 | sed -n '3 p' | awk 'OFS=";" {print $1,$2}'`)
+    hpc=$(cat "$TEMP_OUTPUT" | tr -s " " | cut -d " " -f 2 | tail -n 2 | head -n 1| tr "," ".")
     ##############################################################
 	#############			   OUTPUT				##############
 	##############################################################
-    echo "$timestamp;$cpu;$mem;$network" >> "$FILE_NAME.csv"
+    echo "$timestamp;$cpu;$mem;$network;$hpc" >> "$FILE_NAME.csv"
     #up counter and wait
     x=$(( $x + 1 ))
-    sleep $TIME
 done
 exit 0
