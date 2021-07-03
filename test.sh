8e38547f0067ddb95282287fbe81d10d23ec8aac
Fabio Sisi <45483096+Fabiooo98@users.noreply.github.com>
2021-07-03 10:13:22 +0200

    test new script

--- test.sh
+++ test.sh
@@ -1,8 +1,20 @@
+#!/bin/bash -u
+
+# Set language to make sure same separator (, and .) config is being used
+export LC_ALL=C.UTF-8
+
+# Move to home directory
 cd
 # setup variables and arguments
+# Name of the directory
 DIR_NAME=$1
+# Name of the file
 FILE_NAME="data"
-DIR="/home/fabio/$DIR_NAME"
+# Time window per sample
+timeWindow=10
+# Number of sampoles to be taken
+samples=1000
+
 # check if arguments are given
 if [[ -z "$DIR_NAME" || -z "$FILE_NAME" ]]
 then
@@ -10,21 +22,23 @@ then
     exit -1
 fi
 # check if directory exists, if not then create it
-if [ ! -d "$DIR" ]
+if [ ! -d "$DIR_NAME" ]
 then
     mkdir $DIR_NAME
 fi
+# Enter target directory
 cd $DIR_NAME
 # check if file exists, if not then create it
 if [ ! -f "$DIR/$FILE_NAME.csv" ]
 then
+    # Create the file
     touch "$FILE_NAME.csv"
 fi
 # header line in file
 echo 'time,cpu_usage_pct,ram_usage_pct,network_in,network_out' >> "$FILE_NAME.csv"
 # insert monitoring dimensions in file for 10 seoncds
 x=1
-while [ $x -le 10 ]
+while [ $x -le $samples ]
 do
     printf `date +'%T'";"` >> "$FILE_NAME.csv"
     printf `top -b -n1 | grep "Cpu(s)" | awk '{print $2 + $4}'`";" >> "$FILE_NAME.csv"
@@ -33,7 +47,7 @@ do
     printf "\n" >> "$FILE_NAME.csv"
     #up counter and wait 5 seconds
     x=$(( $x + 1 ))
-    sleep 5
+    sleep $timeWindow
 done
 pwd
 exit 0
